196417ef2754e34b6b192ebb7410d2e835e8248f
JulioSimeon <julio_simeon@dlsu.edu.ph>
2017-08-13 13:16:33 +0800

    added to player class

--- src/bulletHell/Player.java
+++ src/bulletHell/Player.java
@@ -3,12 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bulletHell;
+package breakout;
+
+import acm.graphics.*;
+import java.awt.event.*;
 
 /**
  *
- * @author Paolo
+ * @author jiggy
  */
 public class Player {
+
+    double pHealth = 100;
+    double xCoord = 0;
+    double yCoord = 0;
+    double startX = 0;
+    double startY = 0;
+    GImage pImage = null;
+
+    public void drawPlayer() {
+        pImage = new GImage(insert filename of image ?);
+        add(pImage, insert starting Xcoordinate, insert starting Xcoordinate);
+    }
     
+    public void drawPlayer() {
+        pImage = new GImage(insert filename of image ?);
+        add(pImage, insert starting Xcoordinate, insert starting Xcoordinate);
+    }
+    
+    public void mouseMoved(MouseEvent me){
+        pImage.setLocation(me.getX()-0.5*pImage.getWidth(),me.getY()-0.5*pImage.getHeight());
+    }
+    
+    public void damagePlayer(){
+          if (isPlayerHit())
+              pHealth--;
+    }
+    public boolean isPlayerHit(){
+            if( getElementAt(pImage.getX(),pImage.getY())!=null){
+                return true;
+            }
+            else if ( getElementAt(pImage.getX()+pImage.getWidth(),pImage.getY())!=null){
+                return true;
+            }
+            else if ( getElementAt(pImage.getX(),pImage.getY()+pImage.getHeight())!=null){
+                return true;
+            }
+            else if ( getElementAt(pImage.getX()+pImage.getWidth(),pImage.getY()+pImage.getHeight())!=null){
+                return true;
+            }
+            return false;
+    }
 }
