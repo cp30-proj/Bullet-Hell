473fcd10306b18fbe7771b8d89d8fae94aae2c22
Patthew <Patthew@DESKTOP-7UAATA5>
2019-04-10 00:36:30 +0800

    v2.1
    finished after v2.1 pong
    (ported Paolo's game to Controller Manager)

--- src/supergenericgametitlethegame/SuperGenericGameTitleTheGame.java
+++ src/supergenericgametitlethegame/SuperGenericGameTitleTheGame.java
@@ -31,12 +31,68 @@ public class SuperGenericGameTitleTheGame extends GraphicsProgram implements Sup
     int currentlevel = 1;
     InputStream music;
     
+    
+    ////////////////////////////////////////////////////////////////////////////
+    //////////////////////----- UCMI Connection -----///////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    UCMI ucmi = new UCMI();
+    ControlConfig ctrlConfig;
+    /// mine other
+    int width,height;   //init bounds follows init window size
+    
+    
+    ///////////////////////////////////////////////////////////////
     private GLabel placeholder = new GLabel("Placeholder");
     /**
      * @param args the command line arguments
      */
     
     public void init(){
+        ////////////////////////////////////////////////////////////////////////////
+        //////////////////////----- UCMI Connection -----///////////////////////////
+        ////////////////////////////////////////////////////////////////////////////
+        ucmi.init();
+        if(ucmi.isPortConnected){
+            ucmi.ReqPlayer(1);
+            System.out.println("port connected. 1st requests sent.");
+        }
+        ////////////////////////////////
+        /// defining controls (default)
+        ///////////////////////////////
+        ctrlConfig = new ControlConfig(ucmi);
+        ///////
+        String[] genControlNames = new String[]{"Up","Down","Left","Right","Fire","Open Controls Settings"}; //please insrt new controls at end, unless wiling to change indexing in code.
+        double[]  genDirGrpNo = new double[]{1.1,1.2,1.3,1.4,2,3};
+        ///////
+        ctrlConfig.gameControls = new String[2][]; //player indexing starts at '1'
+        for (int i = 1; i < ctrlConfig.gameControls.length; i++) {
+            ctrlConfig.gameControls[i] =  genControlNames;
+        }
+        ////////
+        ctrlConfig.directionalGroupNo = new double[2][]; //player indexing starts at '1'
+        for (int i = 1; i < ctrlConfig.directionalGroupNo.length; i++) {
+            ctrlConfig.directionalGroupNo[i] = genDirGrpNo;
+        }
+        ////////
+        ctrlConfig.kybdControls = new int[2][]; //player indexing starts at '1'
+        ctrlConfig.kybdControls[1] = new int[]{KeyEvent.VK_UP,KeyEvent.VK_DOWN,KeyEvent.VK_LEFT,KeyEvent.VK_RIGHT,KeyEvent.VK_SPACE,KeyEvent.VK_ESCAPE};    //P1 default
+        ////////
+        ctrlConfig.uartControls = new CM[2][]; //player indexing starts at '1'
+        CM[] genUart = new CM[]{CM.LEFT_ANALOG_STICK_Y,CM.LEFT_ANALOG_STICK_Y,CM.LEFT_ANALOG_STICK_X,CM.LEFT_ANALOG_STICK_X,CM.A_FACE_BUTTON,CM.SELECT_BUTTON};
+        for (int i = 1; i < ctrlConfig.uartControls.length; i++) {
+            ctrlConfig.uartControls[i] = genUart;
+        }
+        ////////
+        ctrlConfig.uartHolddownWaitCount = new int[ctrlConfig.gameControls.length][ctrlConfig.gameControls[1].length];
+        for (int i = 1; i < ctrlConfig.uartHolddownWaitCount.length; i++) {
+            for (int j = 0; j < ctrlConfig.uartHolddownWaitCount[i].length; j++) {
+                ctrlConfig.uartHolddownWaitCount[i][j] = 0;
+            }
+        }
+        
+        
+        ///////////////////////////////
+        ///////////////////////////////
         addKeyListeners();
         addMouseListeners();
         add(Health, NORTH);
@@ -74,27 +130,196 @@ public class SuperGenericGameTitleTheGame extends GraphicsProgram implements Sup
             }
             return false;
    }
-    public void mouseMoved(MouseEvent me){
+    public void mouseMoved(MouseEvent me){/*
                 Player.setXCoordinate(me.getX()-0.5*Player.pImage.getWidth());
-                Player.setYCoordinate(me.getY()-0.5*Player.pImage.getHeight());
+                Player.setYCoordinate(me.getY()-0.5*Player.pImage.getHeight());*/
             }
-    public void keyPressed(KeyEvent ke) {
-            switch (ke.getKeyCode()) {
-                case KeyEvent.VK_E:
-                    tracker.addProjectile(Player.getBullets(FRAME_PAUSE).iterator());
-                    break;
-                default:
-                    break;
-            }
+    final static double PLAYER_SPEED = 15;
+    boolean up = false,dn = false,lf = false,rt = false,fire = false;
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //vvvv Remember: Controls indexing Starts with [0]; players indexing starts with [1].
+        //String[] genControlNames = new String[]{"Up","Down","Left","Right","Start","Select","Quit"}; <--from top-((as of 10:34 pc clk))]
+        int id = e.getKeyCode();
+        //System.out.println("id = " + id);
+        //System.out.println("pressed: "+e.getKeyText(id));
+        //System.out.println("kybdCtrls[1]:"+Arrays.toString(ctrlConfig.kybdControls[1]));
+        //System.out.println("kybdCtrls[2]:"+Arrays.toString(ctrlConfig.kybdControls[2]));
+        
+        if(id == ctrlConfig.kybdControls[1][0]){//p1 up
+            up = true;
         }
+        if(id == ctrlConfig.kybdControls[1][1]){//p1 down
+            dn = true;
+        }
+        if(id == ctrlConfig.kybdControls[1][2]){//p1 left
+            lf = true;
+        }
+        if(id == ctrlConfig.kybdControls[1][3]){//p1 right
+            rt = true;
+        }
+        if(id == ctrlConfig.kybdControls[1][4]){//p1 fire
+            fire = true;
+        }
+    }
+    
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //vvvv Remember: Controls indexing Starts with [0]; players indexing starts with [1].
+        //String[] genControlNames = new String[]{"Up","Down","Left","Right","Start","Select","Quit"}; <--from top-((as of 10:34 pc clk))]
+        int id = e.getKeyCode();
+        
+        if(id == ctrlConfig.kybdControls[1][0]){//p1 up
+            up = false;
+        }
+        if(id == ctrlConfig.kybdControls[1][1]){//p1 down
+            dn = false;
+        }
+        if(id == ctrlConfig.kybdControls[1][2]){//p1 left
+            lf = false;
+        } 
+        if(id == ctrlConfig.kybdControls[1][3]){//p1 right
+            rt = false;
+        } 
+        if(id == ctrlConfig.kybdControls[1][4]){//any select (control settings)
+            fire = false;
+        }
+        if(id == ctrlConfig.kybdControls[1][5]){//any select (control settings)
+            actConSettings();
+        }
+    }
+    
+    ////////////////////////
+    //actions
+    private void shoot(){
+        tracker.addProjectile(Player.getBullets(FRAME_PAUSE).iterator());
+    }
+    
+    private void actConSettings(){  //open control settings
+        if(ctrlConfig.jframe == null  || !ctrlConfig.jframe.isVisible()){
+            ctrlConfig.openSettings(1, 0, 1, 2, 3, 4);
+        }
+    }
+    ////
+    private void moveUp(){
+        double speed = PLAYER_SPEED;
+        if(Player.getYCoordinate()-speed < 0){
+            Player.setYCoordinate(0);
+        } else {
+            Player.setYCoordinate(Player.getYCoordinate()-speed);
+        }
+    }
+    private void moveUp(double percentSpd){
+        double speed = (percentSpd/100*PLAYER_SPEED);
+        if(speed<0){
+            speed = 1;
+        }
+        if(Player.getYCoordinate()-speed < 0){
+            Player.setYCoordinate(0);
+        } else {
+            Player.setYCoordinate(Player.getYCoordinate()-speed);
+        }
+    }
+    ////
+    private void moveDn(){
+        double speed = PLAYER_SPEED;
+        if(Player.getYCoordinate()+Player.pImage.getHeight()+speed > height){
+            Player.setYCoordinate(height-Player.pImage.getHeight());
+        } else {
+            Player.setYCoordinate(Player.getYCoordinate()+speed);
+        }
+    }
+    private void moveDn(double percentSpd){
+        double speed = (percentSpd/100*PLAYER_SPEED);
+        if(speed<0){
+            speed = 1;
+        }
+        if(Player.getYCoordinate()+Player.pImage.getHeight()+speed > height){
+            Player.setYCoordinate(height-Player.pImage.getHeight());
+        } else {
+            Player.setYCoordinate(Player.getYCoordinate()+speed);
+        }
+    }
+    ////
+    private void moveRt(){
+        double speed = PLAYER_SPEED;
+        if(Player.getXCoordinate()+Player.pImage.getWidth()+speed > width){
+            Player.setXCoordinate(width-Player.pImage.getWidth());
+        } else {
+            Player.setXCoordinate(Player.getXCoordinate()+speed);
+        }
+    }
+    private void moveRt(double percentSpd){
+        double speed = (percentSpd/100*PLAYER_SPEED);
+        if(speed<0){
+            speed = 1;
+        }
+        if(Player.getXCoordinate()+Player.pImage.getWidth()+speed > width){
+            Player.setXCoordinate(width-Player.pImage.getWidth());
+        } else {
+            Player.setXCoordinate(Player.getXCoordinate()+speed);
+        }
+    }
+    ////
+    private void moveLf(){
+        double speed = PLAYER_SPEED;
+        if(Player.getXCoordinate()-speed < 0){
+            Player.setXCoordinate(0);
+        } else {
+            Player.setXCoordinate(Player.getXCoordinate()-speed);
+        }
+    }
+    private void moveLf(double percentSpd){
+        double speed = (percentSpd/100*PLAYER_SPEED);
+        if(speed<0){
+            speed = 1;
+        }
+        if(Player.getXCoordinate()-speed < 0){
+            Player.setXCoordinate(0);
+        } else {
+            Player.setXCoordinate(Player.getXCoordinate()-speed);
+        }
+    }
+    
+    
+    ////
+    
     public void run(){
+        //mine
+        width = getWidth();
+        height = getHeight();
+        System.out.println("width = " + width);
+        System.out.println("height = " + height);
+        ////////////////
         tracker.setBounds(getWidth(), getHeight());
-        dialog.println("Click on the screen once then\nPress E to fire");
+        dialog.println("[Up,Dn,Lf,Rt] - Movement \n [Fire] - Shoot Bullets");
         //placeholder();
         while(true){
+            uartReadLoopIterate();
+            /////
             demo();
             tracker.clearLists();
             while(Player.getHealth()>0){
+                //player movement
+                if(up){
+                    moveUp();
+                }
+                if(dn){
+                    moveDn();
+                }
+                if(lf){
+                    moveLf();
+                }
+                if(rt){
+                    moveRt();
+                }
+                if(fire){
+                    shoot();
+                }
+                uartReadLoopIterate();
+                
                 
                 tracker.updateObjects();
                 pause(FRAME_PAUSE);
@@ -112,7 +337,8 @@ public class SuperGenericGameTitleTheGame extends GraphicsProgram implements Sup
                 drawFrame(tracker.getBullets(), tracker.getEnemies());
                 
             }
-            pause(3000);
+            uartLoopExitResetHoldDown();
+            pause(2500);
             tracker.clearLists();
             Player.reset();
         }
@@ -342,6 +568,111 @@ public class SuperGenericGameTitleTheGame extends GraphicsProgram implements Sup
         }
     }
     
+    //////////// UART
+    private void uartReadLoopIterate() {
+        ////////////////////////////////////////////////////////////////////////////
+        //////////////////////----- UCMI Connection -----///////////////////////////
+        ////////////////////////////////////////////////////////////////////////////
+        //uart controls (Movement + Fire)
+        if (ucmi.isPortConnected && !ctrlConfig.disableCMInput){
+            //---- [0],[1]    Up,DN CONTROLS
+            //player control
+            if(ctrlConfig.uartControls[1][0].isAnalogAxis()){
+                int val1 = ucmi.p[1].readAnalogAxis(ctrlConfig.uartControls[1][0]);  //0 to 255
+                val1 = val1 - 128;
+                double percentage = val1;  //(val1/100)*100;      //i made maximum analog stick 100 (instead of 127)
+                if(percentage > 0){
+                    /*if(percentage>100){
+                    percentage = 100;
+                    }*/
+                    moveUp(percentage);
+                } else if (percentage < 0){
+                    percentage = -percentage;
+                    /*if(percentage>100){
+                    percentage = 100;
+                    }*/
+                    moveDn(percentage);
+                }
+            } else {
+                if(ucmi.p[1].readButton(ctrlConfig.uartControls[1][0])){
+                    moveUp();
+                }
+                if(ucmi.p[1].readButton(ctrlConfig.uartControls[1][1])){
+                    moveDn();
+                }
+                
+            }
+            
+            //---- [2],[3]    LF,RT CONTROLS
+            //player control ("LEFT" and "RIGHT") ==> index [2] and [3]
+            if(ctrlConfig.uartControls[1][2].isAnalogAxis()){
+                int val1 = ucmi.p[1].readAnalogAxis(ctrlConfig.uartControls[1][2]);  //0 to 255
+                val1 = val1 - 128;
+                double percentage = val1;  //(val1/100)*100;      //i made maximum analog stick 100 (instead of 127)
+                if(percentage > 0){
+                    /*if(percentage>100){
+                    percentage = 100;
+                    }*/
+                    moveRt(percentage);
+                } else if (percentage < 0){
+                    percentage = -percentage;
+                    /*if(percentage>100){
+                    percentage = 100;
+                    }*/
+                    moveLf(percentage);
+                }
+            } else {
+                if(ucmi.p[1].readButton(ctrlConfig.uartControls[1][2])){
+                    moveRt();
+                }
+                if(ucmi.p[1].readButton(ctrlConfig.uartControls[1][3])){
+                    moveLf();
+                }
+                
+            }
+            
+            
+            //---- [4] Fire
+            if(ucmi.p[1].readButton(ctrlConfig.uartControls[1][4])){
+                shoot();
+            }
+            
+            //---- [5] ConSett [NO repeat press on hold]
+            if(ucmi.p[1].readButton(ctrlConfig.uartControls[1][5])){
+                switch (ctrlConfig.uartHolddownWaitCount[1][5]){
+                    case ControlConfig.UART_HOLD_DOWN_WAIT_TIME:
+                        //actQuit();   [NO repeat press on hold]
+                        break;
+                    case 0:
+                        actConSettings();
+                        ctrlConfig.uartHolddownWaitCount[1][5]++;
+                        break;
+                    default:
+                        ctrlConfig.uartHolddownWaitCount[1][5]++;
+                        break;
+                }
+            } else {ctrlConfig.uartHolddownWaitCount[1][5]=0;}
+        }
+    }
+    
+    
+
+    private void uartLoopExitResetHoldDown() {
+        //////////////////////// UCMI YEAH
+        //since by this point is outside read loop
+        ctrlConfig.uartHolddownWaitCount[1][5]=0;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /////////// MAIN
     public static void main(String[] args) {
         // TODO code application logic here
         new SuperGenericGameTitleTheGame().start(args);
